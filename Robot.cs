e561cc0594b2e91458d80f01858d03f4aa7e25e4
Rob Clark <clarkrob13@gmail.com>
2021-01-11 11:17:46 -0500

    Constructors made for Robot and Dinosaur classes

--- Robot.cs
+++ Robot.cs
@@ -8,5 +8,24 @@ namespace RobotsVsDinosaurs
 {
     class Robot
     {
+        string robotName;
+        int robotHealth;
+        int robotPowerLevel;
+        string robotWeaponType;
+        int robotWeaponPower;
+
+        public Robot(string robotName, int robotHealth, int robotPowerLevel, string robotWeaponType, int robotWeaponPower)
+        {
+            this.robotName = robotName;
+            this.robotHealth = robotHealth;
+            this.robotPowerLevel = robotPowerLevel;
+            this.robotWeaponType = robotWeaponType;
+            this.robotWeaponPower = robotWeaponPower;
+
+        }
+
+
+
+
     }
 }
